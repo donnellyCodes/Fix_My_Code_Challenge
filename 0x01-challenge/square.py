e344854838e1d65491905596e6d5cbac776f4a6b
donnelly19 <donamaitsa@gmail.com>
2024-07-22 13:44:25 -0700

    fix code

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 
 class square():
-    
+
     width = 0
     height = 0
 
-    
-    def __init__(self, *args, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    def is_perfect_square(self):
+        """Check if the shape is a perfect square"""
+        return self.width == self.height
 
     def area_of_my_square(self):
         """ Area of the square """
@@ -20,9 +23,11 @@ class square():
     def __str__(self):
         return "{}/{}".format(self.width, self.height)
 
+
 if __name__ == "__main__":
 
     s = square(width=12, height=12)
     print(s)
+    print(s.is_perfect_square())
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
